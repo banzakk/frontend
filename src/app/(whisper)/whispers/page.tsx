624ee427ec1b7c3f91f1f25b3829d136245eb9c8
Haze10425 <jheee042500@gmail.com>
2023-12-10 23:48:47 +0900

    feat: whispers 페이지 ui 추가

--- src/app/(whisper)/whispers/page.tsx
+++ src/app/(whisper)/whispers/page.tsx
@@ -1,7 +1,34 @@
-export default function Page() {
+import Typography from '@/components/atom/Typography/Typography'
+import cn from './page.module.scss'
+import Post from '@/components/molecule/Post/Post'
+import WhisperList from '@/components/molecule/WhisperList/WhisperList'
+
+export default async function WhispersPage() {
+
   return (
-    <div>
-      whispers page
+    <div className={cn.container}>
+        <div className={cn.topWrapper}>
+          <div className={cn.fixedWrapper}>
+            <div className={cn.titleArea}>
+              <div className={cn.title}>
+                <Typography size="24" weight="800">
+                  타임라인
+                </Typography>
+              </div>
+            </div>
+            <div className={cn.subTitleArea}>
+              <Typography size="14" color="primary" className="subTitle">
+                친구들이 무슨 이야기를 하고 있나요?
+              </Typography>
+            </div>
+            <div className={cn.writeArea}>
+              <Post />
+            </div>
+          </div>
+        </div>
+        <div className={cn.whisperWrapper}>
+          <WhisperList />
+        </div>
     </div>
   )
 }
