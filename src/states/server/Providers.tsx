e44700d0429a6b9affb96eb5e8550365802765b4
Haze10425 <jheee042500@gmail.com>
2023-12-10 23:53:39 +0900

    feat: react query 설정  추가

--- src/states/server/Providers.tsx
+++ src/states/server/Providers.tsx
@@ -8,7 +8,16 @@ interface ProvidersProps {
 }
 
 export default function Providers({ children } : ProvidersProps) {
-  const [queryClient] = React.useState(() => new QueryClient())
+  const [queryClient] = React.useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retryOnMount: true,
+        refetchOnReconnect: false,
+        retry: false,
+      }
+    }
+  }))
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
