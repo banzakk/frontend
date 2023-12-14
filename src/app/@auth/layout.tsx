b6ae49be9e120d50cc5791edae2324417e9727d9
hyunjin <alfma123@naver.com>
2023-12-14 20:37:08 +0900

    feat: 회원가입 layout, page 생성

--- src/app/@auth/layout.tsx
+++ src/app/@auth/layout.tsx
@@ -1,8 +1,7 @@
-import cn from './signup.module.scss'
 export default function MainLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode
 }) {
-  return <section className={cn.container}>{children}</section>
+  return <section>{children}</section>
 }
