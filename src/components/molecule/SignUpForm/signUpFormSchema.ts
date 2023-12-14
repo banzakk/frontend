a04d11e9b1af7de1920863d2813cf88a971c4a02
hyunjin <alfma123@naver.com>
2023-12-14 20:41:10 +0900

    refactor: formSchema 수정

--- src/components/molecule/SignUpForm/signUpFormSchema.ts
+++ src/components/molecule/SignUpForm/signUpFormSchema.ts
@@ -8,14 +8,14 @@ const formSchema = z
     name: z
       .string()
       .min(2, message.minIs('이름은', 2))
-      .max(10, message.maxIs('이름은', 14)),
+      .max(10, message.maxIs('이름은', 10)),
     userCustomId: z
       .string()
       .min(2, message.minIs('아이디는', 2))
       .max(14, message.maxIs('아이디는', 14))
       .regex(
-        /^[^#@\$%\^&]*$/,
-        '아이디에는 #, @, $, %, ^, & 기호를 사용할 수 없습니다.'
+        /^[A-Za-z]+$/,
+        '아이디에는 한글 및 #, @, $, %, ^, & 기호를 사용할 수 없습니다.'
       ),
     password: z
       .string()
@@ -24,11 +24,15 @@ const formSchema = z
         '비밀번호는 최소 8자 이상이며, 대문자, 소문자, 숫자 및 특수 문자를 포함해야 합니다.'
       ),
     passwordCheck: z.string(),
-    email: z.string().email(),
+    email: z.string().email({ message: '유효하지 않은 이메일 형식입니다.' }),
   })
   .refine((data) => data.password === data.passwordCheck, {
     message: '비밀번호가 일치하지 않습니다.',
     path: ['passwordCheck'],
   })
 
-export { formSchema }
+const nonKoreanStringSchema = z
+  .string()
+  .refine((data) => /^[A-Za-z]+$/.test(data))
+
+export { formSchema, nonKoreanStringSchema }
