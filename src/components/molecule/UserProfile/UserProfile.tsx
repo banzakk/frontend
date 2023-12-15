1cdfb377c3ace2e6ceaedd15698ddaa82154c5bf
Haze10425 <jheee042500@gmail.com>
2023-12-15 22:39:49 +0900

    feat: 유저 프로필 더미 데이터 추가

--- src/components/molecule/UserProfile/UserProfile.tsx
+++ src/components/molecule/UserProfile/UserProfile.tsx
@@ -1,39 +1,49 @@
-import React from 'react'
 import Typography from '@/components/atom/Typography/Typography'
 import ProfileImg from '@/components/atom/ProfileImg/ProfileImg'
 import MoreIcon from '@/components/atom/svg/MoreIcon'
 import cn from './UserProfile.module.scss'
+import { User as UserType } from '@/types/user'
 
 const UserProfile: React.FC = () => {
+  const userData: UserType = {
+    userId: '@chopchop',
+    nickname: '춉춉쓰',
+    profileImg: '/images/hamster.jpg',
+    content: '소개글 입력란',
+    follow: '10',
+    follower: '90',
+  }
+
+  const { userId, nickname, profileImg, content, follow, follower } = userData
   return (
     <div className={cn.container}>
       <div className={cn.profileArea}>
-        <ProfileImg src="/images/hamster.jpg" alt="프로필이미지" />
+        <ProfileImg src={profileImg} alt="프로필이미지" />
       </div>
       <div className={cn.nameArea}>
         <div className={cn.nameInfo}>
           <Typography size="18" weight="600">
-            춉춉쓰
+            {nickname}
           </Typography>
           <Typography size="14" color="gray-strong">
-            @chopchop
+            {userId}
           </Typography>
         </div>
         <MoreIcon size="16" color="#C29DFF" />
       </div>
       <Typography size="14" className={cn.content}>
-        소개글 입력란
+        {content}
       </Typography>
       <div className={cn.area}>
         <div className={cn.label}>
           <Typography size="14" weight="600" className={cn.count}>
-            70
+            {follow}
           </Typography>
           <Typography size="14">팔로우</Typography>
         </div>
         <div className={cn.label}>
           <Typography size="14" weight="600" className={cn.count}>
-            986
+            {follower}
           </Typography>
           <Typography size="14">팔로워</Typography>
         </div>
---
+++ src/types/user.ts
@@ -0,0 +1,8 @@
+export type User = {
+  userId: string
+  nickname: string
+  profileImg: string
+  content: string
+  follow: string
+  follower: string
+}
