8115b069c79287529a30baa2de7aefa57b446dd6
hyunjin <alfma123@naver.com>
2023-12-15 23:20:22 +0900

    feat: FormErrorBuidler 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { FieldValues, Path, UseFormReturn } from 'react-hook-form'
+
 type Message = {
   message: string
 }
@@ -36,5 +38,43 @@ export class ValidationMessage {
    */
   maxIs(fieldName: string, max: number): Message {
     return { message: `${fieldName} 최대 ${max}자 이하이어야 합니다.` }
+  }
+}
+
+/**
+ * FormErrorBuilder
+ */
+export class FormErrorBuilder<T extends FieldValues> {
+  private type?: Path<T>
+  private message?: string
+  /**
+   * @param form react-hook-form 의 useForm
+   */
+  constructor(private form: UseFormReturn<T>) {}
+
+  /**
+   * @param type
+   * @returns input 고유의 필드 이름
+   */
+  setType(type: Path<T>): FormErrorBuilder<T> {
+    this.type = type
+    return this
+  }
+  /**
+   * @param message
+   * @returns 인풋의 에러 메시지
+   */
+  setMessage(message: string): FormErrorBuilder<T> {
+    this.message = message
+    return this
+  }
+  /**
+   * @returns form.setError(type, { message })
+   */
+  build() {
+    if (!this.type || !this.message) {
+      throw new Error('타입과 메시지는 반드시 지정되어야 합니다.')
+    }
+    return this.form.setError(this.type, { message: this.message })
   }
 }
