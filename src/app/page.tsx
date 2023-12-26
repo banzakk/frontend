118ae328dd4c52fc95ff0ddb0a78255097d4ef87
hyunjin <alfma123@naver.com>
2023-12-26 22:01:40 +0900

    refactor: Metadata page로 이관

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import SocialLoginConsumer from '@/components/SocialLoginConsumer'
 import { Button } from '@/components/ui/button'
+import { Metadata } from 'next'
 import styles from './root.module.scss'
+
+export const metadata: Metadata = {
+  title: 'Banzakk',
+  icons: { icon: 'http://localhost:3000/favicon.ico' },
+}
 
 export default function Page() {
   return (
