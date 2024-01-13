6ee63a638ba7f39e773172b7c3a3bf3694130382
Haze10425 <jheee042500@gmail.com>
2024-01-13 17:05:13 +0900

    fix: 위스퍼 리스트 props 수정

--- src/components/molecule/WhispersList/WhispersList.tsx
+++ src/components/molecule/WhispersList/WhispersList.tsx
@@ -1,21 +1,12 @@
-'use client'
-
 import Whisper from '@/components/molecule/Whisper/Whisper'
 import { WhisperProps } from '@/types'
 import cn from './WhispersList.module.scss'
-import { useQuery } from '@tanstack/react-query'
-import { getUserWhispers } from '@/services/whisper'
 
-const WhispersList = ({ userId }: { userId: number }) => {
-  const { data } = useQuery<WhisperProps[]>({
-    queryKey: ['user-whispers'],
-    queryFn: () => getUserWhispers(userId.toString()),
-    retry: false,
-  })
+const WhispersList = ({ whispers }: { whispers: WhisperProps[] }) => {
 
   return (
     <ul className={cn.WhisperArea}>
-      {data?.map((whisper) => (
+      {whispers?.map((whisper) => (
         <li key={whisper.whisperId} className={cn.container}>
           <Whisper {...whisper} />
         </li>
