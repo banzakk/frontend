501157d1d673215502b80ade56d87b1267ed5241
Haze10425 <jheee042500@gmail.com>
2024-01-21 18:55:27 +0900

    feat: 댓글란 ui 추가, 상세페이지 이동 추가

--- src/components/molecule/LikeButton/LikeButton.tsx
+++ src/components/molecule/LikeButton/LikeButton.tsx
@@ -5,9 +5,13 @@ import { useState } from "react";
 
 export default function LikeButton({className} : {className:string}) {
   const [isLike, setIsLike] = useState(false);
+  const handleLike = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation()
+    setIsLike(!isLike)
+  }
 
   return (
-    <div onClick={() => setIsLike(!isLike)} className={className}>
+    <div onClick={(e)=>handleLike(e)} className={className}>
       {isLike ? <LikeIcon isFilled={true} /> : <LikeIcon />}
     </div>
   );
--- src/components/molecule/Whisper/Whisper.tsx
+++ src/components/molecule/Whisper/Whisper.tsx
@@ -1,10 +1,10 @@
 'use client'
 
+import { useState } from 'react'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import Typography from '@/components/atom/Typography/Typography'
 import Carousel from '../Carousel/Carousel'
-import CommentIcon from '@/components/atom/svg/CommentIcon'
 import RepostIcon from '@/components/atom/svg/RepostIcon'
 import ShareIcon from '@/components/atom/svg/ShareIcon'
 import Comment from '@/components/molecule/Comment/Comment'
@@ -12,8 +12,15 @@ import LikeButton from '@/components/molecule/LikeButton/LikeButton'
 import DeleteButton from '@/components/molecule/DeleteButton/DeleteButton'
 import { WhisperProps } from '@/types'
 import cn from './Whisper.module.scss'
+import CommentIcon from '@/components/atom/svg/CommentIcon'
+import { useRouter } from 'next/navigation'
 
 export default function Whisper(whisper: WhisperProps) {
+  const [isComment, setIsComment] = useState(false)
+  const handleComment = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation()
+    setIsComment(!isComment)
+  }
   const {
     whisperId,
     nickName,
@@ -47,45 +54,63 @@ export default function Whisper(whisper: WhisperProps) {
     )
   }
   const renderedContent = replaceHashTagWithLink(content, hashTag)
+  const router = useRouter()
+
+  const handleNavigate = () => {
+    router.push(`/whispers/whisper?id=${whisperId}`)
+  }
+
   return (
     <div className={cn.container}>
-      <Carousel PostImages={imageUrl} className={cn.imageArea} />
-      <div className={cn.actionContainer}>
-        <div className={cn.actionGroup}>
-          <CommentIcon className={cn.icon} />
-          <LikeButton className={cn.icon} />
-          <RepostIcon className={cn.icon} />
-          <ShareIcon className={cn.icon} />
-          {isMyWhisper === '1' && <DeleteButton className={cn.icon} />}
+      <div className={cn.linkArea} onClick={handleNavigate}>
+        <Carousel PostImages={imageUrl} className={cn.imageArea} />
+        <div className={cn.actionContainer}>
+          <div className={cn.actionGroup}>
+            <div onClick={(e) => handleComment(e)} className={cn.icon}>
+              <CommentIcon />
+            </div>
+            <LikeButton className={cn.icon} />
+            <RepostIcon className={cn.icon} />
+            <ShareIcon className={cn.icon} />
+            {isMyWhisper === '1' && <DeleteButton className={cn.icon} />}
+          </div>
         </div>
-      </div>
-      <div className={cn.contentContainer}>
-        <div className={cn.nickNameArea}>
-          <Typography size="14" type="div" weight="800" className={cn.nickName}>
-            {nickName}
-          </Typography>
+        <div className={cn.contentContainer}>
+          <div className={cn.nickNameArea}>
+            <Typography
+              size="14"
+              type="div"
+              weight="800"
+              className={cn.nickName}
+            >
+              {nickName}
+            </Typography>
+          </div>
+          <div className={cn.contentArea}>
+            <Typography size="14">{renderedContent}</Typography>
+          </div>
         </div>
-        <div className={cn.contentArea}>
-          <Typography size="14">{renderedContent}</Typography>
-        </div>
+        {comments && comments.length > 0 && (
+          <div>
+            {comments.map((comment) => (
+              <Comment key={comment.commentId} {...comment} />
+            ))}
+            <Button variant="link" className="text-primary" asChild>
+              <Link
+                href={{
+                  pathname: '/whispers/whisper',
+                  query: { id: whisperId },
+                }}
+              >
+                댓글 더 불러오기
+              </Link>
+            </Button>
+          </div>
+        )}
       </div>
-      {comments && comments.length > 0 && (
-      <div>
-        {comments.map((comment) => (
-          <Comment key={comment.commentId} {...comment} />
-        ))}
-        <Button variant="link" className="text-primary" asChild>
-          <Link
-            href={{
-              pathname: '/whispers/whisper',
-              query: { id: whisperId },
-            }}
-          >
-            댓글 더 불러오기
-          </Link>
-        </Button>
+      <div className={cn.commentPostArea}>
+        {isComment && <input className={cn.commentPost}/> }
       </div>
-      )}
     </div>
   )
 }
