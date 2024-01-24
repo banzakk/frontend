58e61406532078106d1055231fcc3bd86f187fe6
Haze10425 <jheee042500@gmail.com>
2024-01-24 23:17:25 +0900

    refactor: svg 컴포넌트 index 추가

--- src/components/atom/svg/index.ts
+++ src/components/atom/svg/index.ts
@@ -2,5 +2,38 @@ import GoogleIcon from './GoogleIcon'
 import KakaoIcon from './KakaoIcon'
 import NaverIcon from './NaverIcon'
 
+import AlertIcon from './AlertIcon'
+import CloseIcon from './CloseIcon'
+import CommentIcon from './CommentIcon'
+import DeleteIcon from './DeleteIcon'
+import HomeIcon from './HomeIcon'
+import LikeIcon from './LikeIcon'
+import MessageIcon from './MessageIcon'
+import MoreIcon from './MoreIcon'
+import RepostIcon from './RepostIcon'
+import SendIcon from './SendIcon'
+import SettingIcon from './SettingIcon'
+import ShareIcon from './ShareIcon'
+import WriteIcon from './WriteIcon'
+import ThemeIcon from './ThemeIcon'
+
 export * from './types'
-export { GoogleIcon, KakaoIcon, NaverIcon }
+export {
+  GoogleIcon,
+  KakaoIcon,
+  NaverIcon,
+  AlertIcon,
+  CloseIcon,
+  CommentIcon,
+  DeleteIcon,
+  HomeIcon,
+  LikeIcon,
+  MessageIcon,
+  MoreIcon,
+  RepostIcon,
+  SendIcon,
+  SettingIcon,
+  ShareIcon,
+  WriteIcon,
+  ThemeIcon
+}
